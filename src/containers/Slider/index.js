ab6063f762a5d20bcb64f31cfe34b9d8fe940926
Susana <hornosu@yahoo.es>
2025-11-20 13:16:31 +0100

    Fix: Slider (both data and display)

--- src/containers/Slider/index.js
+++ src/containers/Slider/index.js
@@ -12,8 +12,7 @@ const Slider = () => {
   );
   const nextCard = () => {
     setTimeout(
-      () => setIndex(index < byDateDesc.length ? index + 1 : 0),
-      5000
+      () => setIndex(index < byDateDesc.length-1 ? index + 1 : 0), 5000
     );
   };
   useEffect(() => {
